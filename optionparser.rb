c5970ec0b29231970f04980bbd3b59432439b9f0
Nandor Biro <nandeej@gmail.com>
2016-05-30 16:10:22 +0200

    set optionparser

---
+++ optionparser.rb
@@ -0,0 +1,58 @@
+require 'optparse'
+require 'ostruct'
+
+class OptparseExample
+
+  #
+  # Return a structure describing the options.
+  #
+  def self.parse(args)
+    # The options specified on the command line will be collected in *options*.
+    # We set default values here.
+    options = OpenStruct.new
+    options.characterset = ['l', 'u', 'n', 's']
+    options.length = 10
+    options.copyclipboard = false
+
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: example.rb [options]"
+
+      opts.separator ""
+      opts.separator "Specific options:"
+
+      opts.on("-u", "--use CHARSET",
+              "Allowed:", "l : lowercase", 'u : uppercase', "n : numbers", "s : special characters") do |chars|
+        options.characterset = chars.split('')
+      end
+
+      opts.on("-l", "--length NUMBER",
+              "Length (mininum: 1, default: 10)") do |l|
+        options.length = l.to_i
+      end
+
+      opts.on("-c", "--copyclipboard", "Copy generated password to clipboard") do |c|
+        options.copyclipboard = c
+      end
+
+      opts.separator ""
+      opts.separator "Common options:"
+
+      # No argument, shows at tail.  This will print an options summary.
+      # Try it and see!
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
+
+      # Another typical switch to print the version.
+      opts.on_tail("--version", "Show version") do
+        puts ::Version.join('.')
+        exit
+      end
+    end
+
+    opt_parser.parse!(args)
+    options
+  end  # parse()
+
+end  # class OptparseExample
--- pwg.rb
+++ pwg.rb
@@ -1,24 +1,31 @@
 #!/usr/bin/env ruby
+require "./optionparser"
 
+Version = [0,0,1]
+
+# Generate password based on given arguments
 class PasswordGenerator
+  ABCL = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z).freeze
+  ABCU = ABCL.map(&:upcase).freeze
+  NUMS = %w(0 1 2 3 4 5 6 7 8 9).freeze
+  SPECCHARS = %w(§ ' " + ! % / = ( ) , . - _ : ? < > { } [ ] # & @ *).freeze
+
   def genpass(length)
-    abcl = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
-    abcu = abcl.map { |x| x.upcase }
-    nums = %w(0 1 2 3 4 5 6 7 8 9)
-    specchars = %w(§ ' " + ! % / = ( ) , . - _ : ? < > { } [ ] # & @ *)
-    arr = [abcl,abcu,nums,specchars]
+    arr = [ABCL, ABCU, NUMS, SPECCHARS]
     pass = []
     length.times do
       choosen_type = arr[rand(0..3)]
-      chossen_char = choosen_type[rand(0..choosen_type.length-1)]
+      chossen_char = choosen_type[rand(0..choosen_type.length - 1)]
       pass << chossen_char
     end
     pass.join('')
   end
 end
 
-if ARGV[0].to_i > 0
-  l = ARGV[0].to_i
+options = OptparseExample.parse(ARGV)
+
+if options[:length] > 0
+  l = options[:length]
   pass = PasswordGenerator.new
   STDOUT.puts "#{l} char password: #{pass.genpass(l)}"
 else
@@ -28,6 +35,14 @@ else
   Usage:
     ruby pwg.rb 10
 
+  Options:
+    -u    characters (eg -u luns)
+            l : lowercase
+            u : uppercase
+            n : numbers
+            s : special characters
+    -l    length (mininum: 1)
+
   EOF
-  puts "Given argument: #{ARGV[0]}"
+  puts "Given argument: #{ARGV}"
 end
