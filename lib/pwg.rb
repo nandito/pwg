70b0a2c2ef4be73f4d517f0f206924f369ea0721
Nandor Biro <nandeej@gmail.com>
2016-06-01 16:06:30 +0200

    create gem which can executed in command line

---
+++ lib/pwg.rb
@@ -0,0 +1,34 @@
+VERSION = [0, 1, 1].freeze
+
+module Pwg
+  # Generate password based on given arguments
+  class PasswordGenerator
+    L = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z).freeze
+    U = L.map(&:upcase).freeze
+    N = %w(0 1 2 3 4 5 6 7 8 9).freeze
+    S = %w(§ ' " + ! % / = ( ) , . - _ : ? < > { } [ ] # & @ *).freeze
+
+    def setcharset(charset)
+      arr = []
+
+      arr << L if charset.map(&:upcase).include?('L')
+      arr << U if charset.map(&:upcase).include?('U')
+      arr << N if charset.map(&:upcase).include?('N')
+      arr << S if charset.map(&:upcase).include?('S')
+
+      arr
+    end
+
+    def genpass(length, charset)
+      arr = setcharset(charset)
+      pass = []
+
+      length.times do
+        choosen_type = arr[rand(0..arr.length - 1)]
+        chossen_char = choosen_type[rand(0..choosen_type.length - 1)]
+        pass << chossen_char
+      end
+      pass.join('')
+    end
+  end
+end
---
+++ pwg.gemspec
@@ -0,0 +1,25 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'pwg'
+
+Gem::Specification.new do |spec|
+  spec.name          = "pwg"
+  spec.version       = ::VERSION.join('.')
+  spec.authors       = ["Nandor Biro"]
+  spec.email         = ["info@nandito.info"]
+
+  spec.summary       = %q{CLI password generator}
+  spec.description   = %q{Generate password in cli}
+  spec.homepage      = "http://nandito.info"
+  spec.license       = "MIT"
+
+  spec.files         = Dir['lib/**/*.rb']
+  #spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.executables   = ["pwg"]
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.11"
+  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rspec", "~> 3.0"
+end
